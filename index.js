060547d05838fe76a412ea736790146c463b90ed
Yevgeniy Valeyev <webmaster.vea@gmail.com>
2022-06-08 08:30:03 +0200

    chore: headers

--- index.js
+++ index.js
@@ -8,12 +8,22 @@ const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, ''); 
+    const method = req.method.toLocaleLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headers = req.headers;
 
     res.end('Hello\n');
 
-    console.log(`Request recieved on path ${trimmedPath}`);
+    const log = [
+        `Request received on path ${trimmedPath}`,
+        `method: ${method}`,
+        `query: ${JSON.stringify(queryStringObject)}`,
+        `headers: ${JSON.stringify(headers)}`,
+    ];
+    console.log(log.map((logItem) => '- '+ logItem).join('\n'));
 });
 
-server.listen(3001, () => {
-    console.log('now on 3000');
+const port = 3001;
+server.listen(port, () => {
+    console.log(`now on ${port}`);
 });
