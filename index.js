d6c4d04e24524011c3c1f4d76cefa42c3ae211e5
Yevgeniy Valeyev <yevgeniy.valeyev@priva.com>
2022-06-08 08:58:41 +0200

    parsing payloads

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 /**
- * Simple server 
+ * Simple server
  */
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 const server = http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, ''); 
-    const method = req.method.toLocaleLowerCase();
-    const queryStringObject = parsedUrl.query;
-    const headers = req.headers;
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
+  const method = req.method.toLocaleLowerCase();
+  const queryStringObject = parsedUrl.query;
+  const headers = req.headers;
 
-    res.end('Hello\n');
+  // get payload
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
 
-    const log = [
-        `Request received on path ${trimmedPath}`,
-        `method: ${method}`,
-        `query: ${JSON.stringify(queryStringObject)}`,
-        `headers: ${JSON.stringify(headers)}`,
-    ];
-    console.log(log.map((logItem) => '- '+ logItem).join('\n'));
+  req.on("data", (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on("end", () => {
+    buffer += decoder.end();
+    res.end("Hello\n");
+    console.log("Payload : ", buffer);
+  });
 });
 
 const port = 3001;
 server.listen(port, () => {
-    console.log(`now on ${port}`);
+  console.log(`now on ${port}`);
 });
